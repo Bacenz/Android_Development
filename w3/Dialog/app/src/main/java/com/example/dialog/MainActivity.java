2e9bf06a0bb216353c343b5d5e60e35286f304e0
Bacenz <bentheuchiha@gmail.com>
2022-11-10 11:32:48 +0700

    add dialog

---
+++ w3/Dialog/app/src/main/java/com/example/dialog/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.dialog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClick(  View view){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog
+                .Builder(MainActivity.this);
+
+        alertDialogBuilder
+                .setTitle("Hello")
+                .setMessage("Do you want to quit?")
+                .setNegativeButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        MainActivity.this.finish();
+                    }
+                })
+                .setPositiveButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                }).create().show();
+    }
+}
